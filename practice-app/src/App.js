29297ae29ec83eb075138d4893158fad528770f6
fosterkelvin <20197992@s.ubaguio.edu>
2023-06-13 11:28:53 +0800

    Components 06/13/2023

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,22 +1,16 @@
-import logo from './bounce.png';
 import './App.css';
+import Navbar from './myComp/navbar.js';
+import AsideList from './myComp/asideList.js';
+import Main from './myComp/main.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Gear 4: Bounce Man</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <AsideList/>
+      <Main/>
     </div>
+
   );
 }
 
---
+++ practice-app/src/myComp/main.js
@@ -0,0 +1,19 @@
+function Main(){
+    return(
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="..." class="d-block w-100" alt="..."></img>
+            </div>
+            <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..."></img>
+            </div>
+            <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..."></img>
+            </div>
+        </div>
+        </div>
+    );
+}
+
+export default Main;
